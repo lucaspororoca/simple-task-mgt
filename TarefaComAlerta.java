9559085d952e88ff136de049be4359ae70b39bca
Lucas Pororoca <lucas.pororoca26@gmail.com>
2024-09-21 00:57:01 +0000

    add: Main.java

--- TarefaComAlerta.java
+++ TarefaComAlerta.java
@@ -1,9 +1,10 @@
 public class  TarefaComAlerta extends Tarefa implements Notificacao{
     public TarefaComAlerta(String titulo, String descricao, String prazo, int prioridade){
         super(titulo, descricao, prazo, prioridade);
+        this.enviarNotificacao();
     }
 
     public void enviarNotificacao(){
-        System.out.println("Tarefa foi enviada");
+        System.out.println("A Tarefa " + super.getTitulo() + " foi enviada");
     }
 }
